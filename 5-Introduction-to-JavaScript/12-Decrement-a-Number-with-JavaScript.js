eb3e563917827b1ccc1df020f8d07f2684df6b73
ClickChapman <alexchap303@gmail.com>
2019-06-18 10:27:40 -0600

    Decremented a number using the -- operator.

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+/*Learned to decrement a number using the -- operator. Much like incrmenting,
+the equation i = i - 1; can be replaced with i--*/
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
