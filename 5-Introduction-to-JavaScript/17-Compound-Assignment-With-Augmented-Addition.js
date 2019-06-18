25fd4b689ebd1859f0abfa84483c0f4a2c1075a9
ClickChapman <alexchap303@gmail.com>
2019-06-18 10:49:20 -0600

    Used += to do mathmatical operation and assignments in one step.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+/*Learned about using the augmented addition (+=) to do a mathmatical operation
+and assignment in one step. */
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
