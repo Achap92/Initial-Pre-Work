6a6320b964b0cbce5e41d4873f96d8b30942baf3
ClickChapman <alexchap303@gmail.com>
2019-06-17 22:52:32 -0600

    used the assignment operator to assign values to our variables.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+// used the assignment operator to assign values to our variables. Assignments
+// go from right to left.
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
