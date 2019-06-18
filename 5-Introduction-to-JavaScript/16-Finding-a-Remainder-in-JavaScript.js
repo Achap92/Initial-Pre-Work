b371b3a13454d49f818aa297d27f94b02a5e8668
ClickChapman <alexchap303@gmail.com>
2019-06-18 10:44:58 -0600

    Used the remainder operator to find the remainder of the division of two numbers.

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,8 @@
+/*Used the % operator to find the remainder of the division of two numbers.
+In mathmatics we can check to see if a number is even or odd by dividing that
+number by 2 where if the remainder is = to 1 then the number is odd and if
+the remainder is = to 0 then the number is even. This however does not work
+properly with negative numbers. */
+// Only change code below this line
+
+var remainder = 11 % 3;
