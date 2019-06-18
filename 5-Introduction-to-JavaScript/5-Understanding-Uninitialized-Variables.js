8d56158c52568be7e2795d6e75ed1daf91468908
ClickChapman <alexchap303@gmail.com>
2019-06-17 22:59:22 -0600

    intialized variables that were previously uninitialized.

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,14 @@
+/*When you create variables in JavaScript they have an inital value of
+undefined so that if we try to do mathmatical operations on an undefined variable
+we will get errors such as NaN(Not a Number) or "undefined" if we try to
+concatenate a string with an undefined variable.*/
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
