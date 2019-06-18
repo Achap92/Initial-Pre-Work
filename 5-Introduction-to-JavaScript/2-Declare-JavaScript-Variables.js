2f2a304de27b242b4c722731d3c4f03bb5e015e3
ClickChapman <alexchap303@gmail.com>
2019-06-17 22:50:07 -0600

    Declared a variable called myName

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,8 @@
+// Created a variable to hold data information we can use at a later time.
+// Much like math, variables are a name we use to represent the data
+// we want to refer to.
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
