96309332ae1388b476c086e3b2765c7ac8e40835
ClickChapman <alexchap303@gmail.com>
2019-06-18 10:25:58 -0600

    Incremented a number using the ++ operator.

---
+++ 5-Introduction-to-JavaScript/11-Increment-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+/*Incremented a number using the increment operator (++). The entire equation
+i = i + 1; can be replaced with i++*/
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
