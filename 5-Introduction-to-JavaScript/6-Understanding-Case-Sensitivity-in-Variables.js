4d2011e440d98559e643375e5e71a2a336d329e8
ClickChapman <alexchap303@gmail.com>
2019-06-17 23:04:57 -0600

    Understand how case sensitivity works.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,12 @@
+// in JavaScript spelling is very important. To avoid any errors it is best
+// practice to use camelCase in the the first word is all lower case and the
+// rest of the subsequent words have a capital starting letter.
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
