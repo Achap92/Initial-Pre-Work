4cf3e772f287a6285082ef1112773aece26141b1
ClickChapman <alexchap303@gmail.com>
2019-06-19 09:32:59 -0600

    Created a function in JavaScript.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,16 @@
+/*Created a function that we can then reuse in our code. To invoke the function
+we must call it after the function definition using the same name as the function
+definition. */
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
