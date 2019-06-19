ba46c39a0ef934bafd7435ff1d4c3e140bcfc15a
ClickChapman <alexchap303@gmail.com>
2019-06-18 22:33:55 -0600

    Stored multiple values in an array separated by a comma.

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+/*Learned to store multiple values in a variable using an array. Values must be
+separated by a comma.*/
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Ginger Snaps", 4, "Coconut", "Pickles"];
