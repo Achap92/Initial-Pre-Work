74c3f5cd655b19c7090317e88f8f379d88c84fa4
ClickChapman <alexchap303@gmail.com>
2019-06-19 10:46:00 -0600

    Used the inequality operator to determine if a value is not equal to a number given.

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+/*Used the inequality operator (!=) to determine if val is not equal to a numebr
+specified. If it is not then we return true. If it is then we return false.*/
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual("bob");
