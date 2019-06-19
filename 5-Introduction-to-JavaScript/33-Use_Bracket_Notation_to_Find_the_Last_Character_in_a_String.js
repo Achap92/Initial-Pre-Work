a1b37c213630d9ebe6977fe499fe77a162c9f3dc
ClickChapman <alexchap303@gmail.com>
2019-06-18 22:11:05 -0600

    Found the last letter of a string by subtracting 1 from the .length function attached to a variable.

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+/*Learned to find the last letter of a string by subtracing 1 from the .length
+function attached to the variable.*/
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
