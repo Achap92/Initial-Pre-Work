8d11c7467491bda9f22dc21175aacdb173823448
ClickChapman <alexchap303@gmail.com>
2019-06-18 21:03:35 -0600

    created a string literal variable in JavaScript

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+/*Declared a string variable. Using the quotation marks creates a string literal
+because it is a series of zero or more characters enclosed inside.*/
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Alex";
+var myLastName = "Chapman";
