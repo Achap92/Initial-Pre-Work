8f41eb6cdf594c302a3cac86ad68a9d6606b9bec
ClickChapman <alexchap303@gmail.com>
2019-06-18 21:27:48 -0600

    Created a string using a variable assigned to a string.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+/*Created a string using a variable. */
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Alex Chapman";
+var myStr = "My name is " + myName + " and I am well!";
