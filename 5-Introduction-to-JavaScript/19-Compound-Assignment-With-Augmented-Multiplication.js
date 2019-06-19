049e9e51a9c4caa9a3b0d6dfba8c48c80b70b6af
ClickChapman <alexchap303@gmail.com>
2019-06-18 20:58:20 -0600

    Multiplied and assigned a number with the *= operator

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,10 @@
+/*Used the *= operator to multiply and assign a numebr at the same time.*/
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
