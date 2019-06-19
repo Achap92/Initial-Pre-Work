8323501536cedb444c34e6c8331f82b63d3d1c3a
ClickChapman <alexchap303@gmail.com>
2019-06-18 22:13:00 -0600

    Found the nth to last letter of a string in JavaScript

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+/*Used bracket notation to find the nth to last letter of a string using the
+.length function attached to a variable.*/
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
