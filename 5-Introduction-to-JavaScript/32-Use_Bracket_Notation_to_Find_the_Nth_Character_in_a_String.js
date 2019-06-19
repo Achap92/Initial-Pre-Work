239911110a37e2a17e6664f20778711ab5f0ab53
ClickChapman <alexchap303@gmail.com>
2019-06-18 22:09:02 -0600

    Found the nth letter of a string.

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+/*Used bracket notation to the the nth letter of a string keeping in mind of the
+zero based indexing JavaScript uses. */
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
