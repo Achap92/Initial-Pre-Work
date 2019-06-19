6c34a0ae5969836f793ada280dd60914f310a766
ClickChapman <alexchap303@gmail.com>
2019-06-18 21:09:15 -0600

    Used backslash to create a quote within a string.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,5 @@
+/*Learned to use the backslash symbol in front of a quote that is inside
+quotation marks to tell JavaScript that I want to use a quote inside of a quote.
+Doing this tells JavaScript that the following quote is not the end of the string
+but should appear inside the string.*/
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
