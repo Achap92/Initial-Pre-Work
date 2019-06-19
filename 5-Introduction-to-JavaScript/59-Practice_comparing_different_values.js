462a9c6c8a984932a81de58e7ff8bb29a6021cef
ClickChapman <alexchap303@gmail.com>
2019-06-19 10:41:23 -0600

    More practice comparing values with the strict equality operator (===)

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,11 @@
+/*Practiced with more equality comparisons*/
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(20, "20");
