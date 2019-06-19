11d305efc5ae9a3af34274f8ccf4a7856725c6ba
ClickChapman <alexchap303@gmail.com>
2019-06-18 22:39:44 -0600

    Accessed the first element in an array using JavaScript

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+/*Accessed the index of an array. Note that indexes start at 0 and go from
+there. */
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
