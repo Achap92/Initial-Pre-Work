cc177b539fa5c92c9136fa2bfbfde26c806093a5
ClickChapman <alexchap303@gmail.com>
2019-06-19 11:15:14 -0600

    Made a comparison using the strict inequality operator.

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+/*Used the strict inequality operator (!==) to determine if a value is
+strictly equal to the conditional.*/
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual("Alex");
