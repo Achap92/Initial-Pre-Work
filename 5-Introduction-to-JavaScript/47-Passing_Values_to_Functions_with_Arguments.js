d09064b73f23159a16780fca0941057c5ea8bb94
ClickChapman <alexchap303@gmail.com>
2019-06-19 09:37:41 -0600

    Passed in values to a function with arguments.

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,15 @@
+/*Added two parameters to a function then passed in arguments to assign the
+parameters to those arguments.*/
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(i, j) {
+  console.log(i + j);
+}
+
+functionWithArgs(3, 7);
