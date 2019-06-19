ba7074c3132f379125badb8b3ebf7605f898ca3e
ClickChapman <alexchap303@gmail.com>
2019-06-19 09:26:36 -0600

    Used unshift() to add an element to the front of an array.

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,15 @@
+/*Learned to add elements to an array using the unshift() function. This work like
+push() but instead of added the elements to the end, unshift() adds them to them
+front.*/
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35]);
