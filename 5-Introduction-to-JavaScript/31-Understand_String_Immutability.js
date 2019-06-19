64146a09d2a08223d6ed8c434ee9c13d7fa93eaa
ClickChapman <alexchap303@gmail.com>
2019-06-18 22:07:26 -0600

    Changed the value of a string by declaring a new string to that variable

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+/*Learned that string value are immutable in JavaScript. The only way to change
+a string literal is to assign the variable with a new string.*/
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
