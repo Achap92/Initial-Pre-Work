9b0c679042e48f1c0794d133d22204e6c7b6a075
ClickChapman <alexchap303@gmail.com>
2019-06-18 21:31:38 -0600

    Foud the length of a string.

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,14 @@
+/*Learned to see how long a string is by using the .length function.*/
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
