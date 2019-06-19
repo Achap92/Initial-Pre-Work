258cc3de61806f320697acace7b43968e765a5ac
ClickChapman <alexchap303@gmail.com>
2019-06-18 21:25:11 -0600

    Used the += operator to concatenate strings together.

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,11 @@
+/*Used the += operator to concatenate strings together. Note that concatenation
+does not add the spaces for proper sentence structure, I need to add them
+myself.*/
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
