c01f208a95651c368b71262f8ad4b9a455aa4da5
ClickChapman <alexchap303@gmail.com>
2019-06-18 22:47:53 -0600

    Accessd multi-dimensional arrays with indexes.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,8 @@
+/*Learned to access multi-dimensional arrays with indexes. The first set of brackets
+refers to the entries in the outer level of the array and each bracket after
+refers to the next level of entries.*/
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
