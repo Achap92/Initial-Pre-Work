9fb3bb87f41ac9ac77c59a7a98aaa6416f77561c
ClickChapman <alexchap303@gmail.com>
2019-06-19 10:28:34 -0600

    Returned a true boolean value in JavaScript.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+/*Learned about boolean values. These either equate to true(on) or false(off).*/
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
