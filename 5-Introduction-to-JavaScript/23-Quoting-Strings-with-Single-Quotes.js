9fd033a6fd39fce30d10fbfaf345f1a30a1a1dab
ClickChapman <alexchap303@gmail.com>
2019-06-18 21:15:35 -0600

    Used single quotes so that I can use double quotes in the string without using escape characters.

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,6 @@
+/*Learned how to quote strings with single quotes. This is helpful when wanting
+to avoid using escape characters so that you can use both types of quotation
+marks in a string. Be careful to not add another single quote in the middle
+or JavaScript will think that is the end of the quote and will throw an error
+in that case you would need to place the escape character in the quote.*/
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
