1a04eab3b2f2f681e0f7da09d324ca68d2a44dd4
ClickChapman <alexchap303@gmail.com>
2019-06-20 10:58:58 -0600

    Used logical order in if, else if statements.

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,14 @@
+/*Using the correct order is important when using if, else if statements. Function
+is executed from the top to the bottom.*/
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
