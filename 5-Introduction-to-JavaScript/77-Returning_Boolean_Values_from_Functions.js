a6999530b93743d8d31f0a8152ba2f941b041d75
Alex Chapman <Alexchap303@gmail.com>
2019-06-21 09:38:53 -0600

    Returned a boolean value from a function using an equality comparison.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,11 @@
+/*Returned a boolean value from a function. Equality operations return either
+true or false so in the event we create an if/else chain that compares a to b
+ that we want to return true or false we can simplify the if/else chain and
+ return the comparison instead.  */
+ function isLess(a, b) {
+  // Fix this code
+  return a <= b;
+}
+
+// Change these values to test
+isLess(6, 19);
