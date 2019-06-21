063e0e791dc00e87a16b8ae1531a7e0e92058f1a
Alex Chapman <Alexchap303@gmail.com>
2019-06-21 08:44:51 -0600

    Created multiple identical options in a switch statement

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,33 @@
+/*Learned to create multiple identical options in a switch statement. When
+the break statement is omitted from a case the following case statement are
+executed until a break is encountered.*/
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      return "Low";
+      break;
 
+    case 4:
+    case 5:
+    case 6:
+      return "Mid";
+      break;
+
+    case 7:
+    case 8:
+    case 9:
+      return "High";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);
