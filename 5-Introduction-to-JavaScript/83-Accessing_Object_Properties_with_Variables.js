6b92382fec5a42b0fd890a5d6f4808e076cc040a
Alex Chapman <Alexchap303@gmail.com>
2019-06-23 10:16:06 -0600

    Accessed an object property by using a variable

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+/*Accessed object properties with a variable. This can be useful when
+iterating through an objects properties or when accessing a lookup table.*/
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
