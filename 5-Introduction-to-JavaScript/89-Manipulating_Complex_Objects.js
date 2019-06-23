7a6306a88a9ab0dfc47dab5eb5159c5645298f4d
Alex Chapman <Alexchap303@gmail.com>
2019-06-23 12:11:26 -0600

    Created a flexible data structure

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,29 @@
+/*Learned to create a flexible data structure. A JavaScript object has the ability
+to hande flexible data with arbitrary combinations of strings, number, booleans,
+arrays, functions, and objects. */
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+  {
+  // Add record here
+  "artist": "John Dener",
+    "title": "Take Me Home, Country Roads",
+    "release_year": 1971,
+    "formats": [
+      "CD",
+      "8T",
+      "LP",
+      "7-inch Single"
+    ],
+  }
+];
