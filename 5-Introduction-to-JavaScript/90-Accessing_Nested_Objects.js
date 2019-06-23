888a9c0f8136acf91f76de368a83d1b9ae107955
Alex Chapman <Alexchap303@gmail.com>
2019-06-23 12:35:25 -0600

    Accessed sub-properties of an object using dot and bracket notation.

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+/*Accessed the sub-properties of an object by chaining together dot and
+bracket notation.*/
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
