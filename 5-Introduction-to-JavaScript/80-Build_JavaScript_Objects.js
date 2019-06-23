e5bd365dda69e37edcd9db836d13bf4d5e5cb5fa
Alex Chapman <Alexchap303@gmail.com>
2019-06-23 09:47:57 -0600

    Built JavaScript objects.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,19 @@
+/*Created objects in Javascript. Objects are similar to arrays only instead
+of using indexes to access the data you use properties. This is useful for storing
+data in a structured way can sometimes represent real world objects.*/
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  name: "Bandit",
+  legs: 4,
+  tails: 1,
+  friends: ["Everyone!"]
+};
