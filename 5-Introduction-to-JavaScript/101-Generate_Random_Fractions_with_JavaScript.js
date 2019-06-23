5386956c38e5d6197c4b5b6cf7e1287ab0db32fe
Alex Chapman <Alexchap303@gmail.com>
2019-06-23 16:36:52 -0600

    Generated random decimals with Math.random()

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+/*Learned about the Math.random() function to return a random decimal number
+between 0(inclusive) and 1(exclusive) this means that it can return a 0 but
+never qute return a 1*/
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+  // Only change code above this line.
+}
