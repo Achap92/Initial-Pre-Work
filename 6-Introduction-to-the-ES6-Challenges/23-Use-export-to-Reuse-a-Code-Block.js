406225fcca864b91ac225940b89477f0a37c64d8
Alex Chapman <Alexchap303@gmail.com>
2019-06-25 09:49:03 -0600

    Used export to reuse code.

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,5 @@
+/*Learned to export code blocks from a file. We will need to export pieces of code
+that we want to then later import into other code files.*/
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
