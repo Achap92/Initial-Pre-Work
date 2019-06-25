3a071bf1618e7a0ccf7d32ca3aae81060c7875dd
Alex Chapman <Alexchap303@gmail.com>
2019-06-25 09:44:57 -0600

    Used import to import a function from another code file.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,6 @@
+/*Learned about import. This can help when you want to use functions from
+other code files but dont want to use the entire file. Import allows us to select
+certain functions that we need to use in our current code file.*/
+import { capitalizeString } from "string_functions"
+"use strict";
+capitalizeString("hello!");
