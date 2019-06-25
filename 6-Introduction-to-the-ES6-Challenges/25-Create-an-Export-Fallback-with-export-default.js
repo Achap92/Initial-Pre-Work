7628b0c2e3076a57f84665ec3daa29cce5be2d58
Alex Chapman <Alexchap303@gmail.com>
2019-06-25 09:55:35 -0600

    Created an export fallback with export default.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,5 @@
+/*Learned to create an exprort fallback. Usually you will use this syntax if only
+one value is being exported from a file. It is also used to create a fallback
+value for a file or module.*/
+"use strict";
+export default function subtract(x,y) {return x - y;}
